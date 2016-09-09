2bc7bfe39d5e2052ef4aab87e0b4e8b385db2aca
lprimak <lenny@flowlogix.com>
2016-09-08 22:17:00 -0500

    checking local server via ORBImpl by checking against all local interfaces

--- orbmain/src/main/java/com/sun/corba/ee/impl/ior/iiop/IIOPAddressBase.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/ior/iiop/IIOPAddressBase.java
@@ -46,14 +46,9 @@ abstract class IIOPAddressBase implements IIOPAddress
         return value ;
     }
     
-    protected boolean isLocalServer() {
-        return false;
-    }
-
     @Override
     public void write( OutputStream os )
     {
-        os.write_boolean(isLocalServer());
         os.write_string( getHost() ) ;
         int port = getPort() ;
         os.write_short( intToShort( port ) ) ;
@@ -80,6 +75,6 @@ abstract class IIOPAddressBase implements IIOPAddress
     @Override
     public String toString()
     {
-        return String.format("IIOPAddress%s[%s, %d]", isLocalServer()? "(local)" : "", getHost(), getPort());
+        return String.format("IIOPAddress[%s, %d]", getHost(), getPort());
     }
 }
--- orbmain/src/main/java/com/sun/corba/ee/impl/ior/iiop/IIOPAddressImpl.java
+++ orbmain/src/main/java/com/sun/corba/ee/impl/ior/iiop/IIOPAddressImpl.java
@@ -35,7 +35,6 @@ public final class IIOPAddressImpl extends IIOPAddressBase
 
     private String host;
     private int port;
-    private boolean isLocalServer = false;
     
     public IIOPAddressImpl( String host, int port ) 
     {
@@ -49,7 +48,6 @@ public final class IIOPAddressImpl extends IIOPAddressBase
 
     public IIOPAddressImpl( InputStream is )
     {
-        isLocalServer = is.read_boolean();
         host = is.read_string() ;
         short thePort = is.read_short() ;
         port = shortToInt( thePort ) ;
@@ -65,10 +63,5 @@ public final class IIOPAddressImpl extends IIOPAddressBase
     public int getPort()
     {
         return port ;
-    }
-    
-    @Override
-    public boolean isLocalServer() {
-        return isLocalServer;
-    }
+    }    
 }
